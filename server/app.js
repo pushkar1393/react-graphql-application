cc9d37a84afc777e20f934dbeb0ea001d7966edf
Pushkar Khedekar <pushkarkhedekar@gmail.com>
2019-01-26 17:01:25 -0500

    Added few components

--- server/app.js
+++ server/app.js
@@ -3,11 +3,16 @@ const graphqlHTTP = require("express-graphql");
 const chalk = require("chalk");
 const schema = require("./schema/schema");
 const mongoose = require("mongoose");
+const cors = require("cors");
 
 require("dotenv").config();
 
 const app = express();
 
+//allow cross-orgin access
+app.use(cors());
+
+//connections to the database (mlab in this case)
 mongoose.connect(`mongodb://${process.env.USER_NAME}:${process.env.USER_PASS}@ds155774.mlab.com:55774/graphql_book_app`);
 mongoose.connection.once('open',()=>{
   console.log(chalk.blue("Connected to the database"));
